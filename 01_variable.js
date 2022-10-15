12c8e9e176ec7a6323d928fe284c665b77c030ca
isdiscodead <isdiscodead@korea.ac.kr>
2022-10-16 01:17:19 +0900

    -

--- 01_variable.js
+++ 01_variable.js
@@ -11,6 +11,7 @@ console.log(name)
 var name = "Jane";
 
 
+
 /*
 생성자 함수
 */
@@ -29,6 +30,7 @@ let user1 = new User('Mike', 30);
 let user2 = new User('Jane', 22);
 let user3 = new User('Tom', 17);
 user3.sayName();
+
 
 
 /*
@@ -71,6 +73,7 @@ const newUser = Object.fromEntries(arr);
 console.log(newUser);
 
 
+
 /*
 Symbol 심볼 -> 유일한 식별자를 만들 때 사용함 ! log 직으면 다 Symbol()로 뜨지만 내용물은 다름
 */
@@ -93,6 +96,7 @@ Reflect.ownKeys(symbolUser); // symbol 키를 포함한 모든 key 리스트
 id1 = Symbol.for('id'); // 하나를 생성한 뒤 키를 통해 같은 Symbol 공유
 id2 = Symbol.for('id'); // id1 === id2
 console.log(Symbol.keyFor(id1)); 
+
 
 
 /*
@@ -151,3 +155,9 @@ parseInt(padding); // 18 -> 소수점 이하 무시
 
 // 1 ~ 100 사이의 랜덤 숫자 뽑기
 Math.floor( Math.random() * 100 ) + 1;
+
+
+
+/*
+String 
+*/
