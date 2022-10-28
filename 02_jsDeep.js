a7be6fb3f88095cc5b17cfd4e7e58dcfd7b9abfb
isdiscodead <isdiscodead@korea.ac.kr>
2022-10-29 00:33:53 +0900

    closure

--- 02_jsDeep.js
+++ 02_jsDeep.js
@@ -1,4 +1,39 @@
 /*
 클로저 Closure 
 */
-// 어휘적 환경 Lexical Environment
+
+// 어휘적 환경( Lexical Environment ) → 선언 여부에 따라 Lexical 환경에 저장된 내용 달라짐 … 함수나 코드 블록마다 각각의 Lexical 환경이 생성됨
+// - `전역 Lexical 환경` : 스크립트 → 최상위 외부 렉시컬 환경
+// - `내부 Lexical 환경` : 전역 Lexical 환경을 참조, 지역 변수나 매개변수 저장
+
+// `클로저( Closure )` → 함수와 렉시컬 환경의 조합 ; 함수가 생성될 당시의 외부 변수를 기억 → 생성 이후에도 계속 접근 가능
+
+function makeAdder(x) {
+	return function(y) {
+        return x + y;
+	}
+}
+
+const add3 = makeAdder(3);
+// add3 함수가 생성된 이후에도 상위 함수인 makeAdder의 x에 접근 가능해짐 
+console.log(add3(2)); // makeAdder(3) -> function(2) { return 3 + 2 } ... 5
+
+// add10과 add3은 서로 다른 렉시컬 환경을 가짐 
+const add10 = makeAdder(10);
+console.log(add10(5)); // 15
+console.log(add3(1)) // 4
+
+// 카운터 예제 -> 은닉화 가능 
+function makeCounter() {
+    let num = 0; // 생성 후 계속 사용될 외부 변수 
+    
+    return function() {
+        return num++;
+    };
+}
+
+let counter = makeCounter();
+
+console.log(counter()); // 0
+console.log(counter()); // 1
+console.log(counter()); // 2
