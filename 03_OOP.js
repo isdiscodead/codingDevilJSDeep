964ca592cb3e018e8d11190e8c3a6c463e71a7ff
isdiscodead <isdiscodead@korea.ac.kr>
2022-11-03 17:19:38 +0900

    oop end

--- 03_OOP.js
+++ 03_OOP.js
@@ -47,3 +47,60 @@ BMW.prototype.wheels = car.wheels;
 BMW.prototype.drive = car.drive;
 
 const x5 = new BMW("pink");
+
+
+/*
+Class -> ES6에 추가된 스펙
+*/
+class User2 {
+	constructor(name, age) {
+		this.name = name;
+		this.age = age;
+	}
+
+	showName() {
+		console.log(this.name);
+	}
+}
+
+// new 키워드로 constructor 호출, 객체 생성
+const tom = new User2("Tom", 19); // User2의 프로토타입 가짐
+
+
+// 상속 extends
+// extends 키워드를 통해 상속 가능
+class Car {
+    constructor(color) {
+        this.color = color;
+        this.wheels = 4;
+    }
+
+    drive() {
+        console.log("drive..");
+    }
+    stop() {
+        console.log("STOP!");
+    }
+}
+
+class Bmw extends Car {
+    park() {
+        console.log("PARK");
+    }
+}
+
+const z4 = new Bmw("Blue"); 
+
+
+// 오버라이딩 ( overriding )
+class Bmw2 extends Car {
+    park() {
+        console.log("PARK");
+    }
+    stop() {
+        super.stop(); // 부모의 기능도 사용
+        console.log("OFF"); 
+    }
+}
+
+z4 = new Bmw2("Blue"); 
