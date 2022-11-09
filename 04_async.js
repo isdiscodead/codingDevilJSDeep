9f563f4c3a705e10e2f12442be6d0540e845ece2
isdiscodead <isdiscodead@korea.ac.kr>
2022-11-09 17:33:31 +0900

    yield

--- 04_async.js
+++ 04_async.js
@@ -141,9 +141,92 @@ const f3 = () => {
 // Promise.then()보다 높은 가독성 ! 
 console.log("시작");
 async function order() {
+	// const result = await Promise.all([f1(), f2(), f3()]); 
 	const result1 = await f1();
 	const result2 = await f2(result1);
 	const result3 = await f3(result2);
 	console.log(result3);
 	console.log("종료");
+}
+
+
+/*
+Generator -> 함수의 기능을 중간에 멈췄다가 다시 실행 가능
+*과 yield 키워드 사용 !! Generator 함수 사용시 Generator 객체 반환됨 
+.next()로 yield문을 기점으로 실행 가능 
+*/
+function* fn() {
+	console.log(1);
+	yield 1; // value:1 , done: false
+	console.log(2);
+	yield 2; // value:2 , done: false
+	console.log(3);
+	console.log(4);
+	yield 3;
+	return "finish"; // value: "finish", done: true
+}
+
+const a = fn();
+a.next(); 
+a.next(); 
+a.next(); 
+
+// 즉시 종료
+a.return('END');
+a.throw(); // catch 블록에 있는 내용 실행, done을 true로 
+
+// generator는 iterable ( 반복 가능 )이면서 iterator ...
+// 배열도 iterable하며, 아래와 같은 특징을 가지지만 사용하지 않을 뿐 ! 
+// 1. Symbol.iterator 메서드 존재
+// 2. Symbol.iterator는 iterator 반환 
+
+// iterator
+// 1. value와 done 속성을 가지는 객체를 반환하는 next()를 가짐 
+// 2. 작업이 끝나면 done은 true가 됨 
+
+// 인수를 입력 받을 수도 있음
+function* fn2() {
+	const num1 = yield "첫번째 숫자를 입력해주세요";
+	console.log(num1);
+
+	const num2 = yield "두번째 숫자를 입력해주세요";
+	console.log(num2);
+
+	return num1 + num2;
+}
+
+const b = fn();
+
+b.next(); // value: 첫번째 숫자를 입력해주세요 
+b.next(2); // num1: 2
+b.next(); // value: 두번째 숫자를 입력해주세요
+b.next(4); // num2: 4
+b.next(); // value: 6, done: true
+
+// generator는 값을 미리 만들어두지 않음 !! 따라서 무한 반복도 가능 
+function* infinite() {
+	let index = 0;
+	while ( true ) {
+		yield index ++;
+	}
+}
+
+const c = fn();
+// 원하는 만큼 c.next() 사용 가능
+c.next(); // 1
+c.next(); // 2
+
+// yield* 로 다른 제네레이터 호출 가능
+function* gen1() {
+	yield "w";
+	yield "o";
+	yield "r";
+	yield "l";
+	yield "d";
+}
+
+function* gen2() {
+	yield "Hello, ";
+	yield* gen1(); // 반복 가능한 모든 객체가 올 수 있음 
+	yield "!"; 
 }
